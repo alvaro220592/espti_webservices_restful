634d53e284bc536fbc2a7df9c323f62e44440a81
alvaro220592 <alvaro220592@gmail.com>
2022-08-04 20:25:07 -0300

    ...

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,74 +2,53 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     private $category;
 
-    public function __constructor(Category $category){
-        $this->category = $category;
+    public function __construct(Category $category){
+        $this->category = $category;    
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        return response()->json($this->category->getResults($request->name));
+    public function index(Request $request){
+        return $this->category->getResults($request->name);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        if($this->category->create($request->all())){
-            return response()->json(['mensagem' => 'Categoria salva com sucesso'], 201);
-        }else{
-            return response()->json(['mensagem' => 'Erro ao cadastrar'], 500);
+    public function store(Request $request){
+        if ($this->category->create($request->all())) {
+            return response()->json('Cadastrado com sucesso', 201);
+        } else {
+            return response()->json('Erro ao cadastrar', 500);
+        }        
+    }
+
+    public function update(Request $request, $id){
+        $category = $this->category->find($id);
+        
+        if(!$category)
+            return response()->json('Categoria não encontrada', 400);
+        
+        if ($category->update($request->all())) {
+            return response()->json('Editado com sucesso', 201);
+        } else {
+            return response()->json('Erro ao editar', 500);
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    public function destroy($id){
+        $category = $this->category->find($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if(!$category)
+            return response()->json('Categoria não encontrada', 404);
+        
+        if ($category->delete()) {
+            return response()->json('Deletado com sucesso', 200);
+        } else {
+            return response()->json('Erro ao deletar', 500);
+        }        
     }
 }
